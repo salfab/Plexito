3906b0501030325a1931d61aac0a29bdbdd834d7
Fabio Salvalai <fabio.salvalai@frontiersin.org>
2015-09-07 14:38:43 +0200

    Introduce MVVM for Now Playing information on the Main Window.

---
+++ src/Plexito/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Plexito.Annotations;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Plexito.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string _parent;
+        private string _title;
+
+        public MainWindowViewModel()
+        {
+            Title = "Title";
+            Parent = "Parent";
+        }
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                if (value == _title) return;
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Parent
+        {
+            get
+            {
+                return _parent;
+            }
+            set
+            {
+                if (value == _parent) return;
+                _parent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
