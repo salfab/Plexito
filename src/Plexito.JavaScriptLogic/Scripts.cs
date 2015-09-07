d180d2f648c7df6134bcde2fa47f5d4bb1f2c5cf
Fabio Salvalai <fabio.salvalai@frontiersin.org>
2015-09-07 11:33:59 +0200

    introduce javascript implementations of the binding to the main Plex API  (GetDevices) + INTRODUCE XML DOM PARSER :) :)

--- src/Plexito.JavaScriptLogic/Scripts.cs
+++ src/Plexito.JavaScriptLogic/Scripts.cs
@@ -30,7 +30,35 @@
                     result += reader.ReadToEnd();
                 }
                 return result;
-            }            
+            }
+        }
+
+        public static string PlexApi
+        {
+            get
+            {
+                // make it Lazy<string>.
+
+                var assembly = Assembly.GetExecutingAssembly();
+                var resourceName = "Plexito.JavaScriptLogic.JavaScript.PlexApi.js";
+
+                string result;
+
+                using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    result = reader.ReadToEnd();
+                }
+
+                resourceName = "Plexito.JavaScriptLogic.JavaScript.PlexApiForJint.js";
+
+                using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    result += reader.ReadToEnd();
+                }
+                return result;
+            }
         }
     }
 }
